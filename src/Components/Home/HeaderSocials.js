39b22b31d4df4191666a6c26bfd12f2487fa523a
Amit Kumar Goswami <cseiubatgoswami@gmail.com>
2023-04-18 01:58:38 +0600

    new

--- src/Components/Home/HeaderSocials.js
+++ src/Components/Home/HeaderSocials.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInstagram, faTwitter, faBehance, faDribbble, faPinterest } from '@fortawesome/free-brands-svg-icons';
+import { faYoutube,  faTwitter, faLinkedin, faDribbble, faPinterest } from '@fortawesome/free-brands-svg-icons';
 
 
 const HeaderSocials = () => {
   return (
     <div className='home__socials'>
-        <a href='https://www.instagram.com' className='home__social-link' target="_blank">
-            <FontAwesomeIcon icon={faInstagram} />
+        <a href='https://www.youtube.com/@CodingWithGoswami-lw6cz' className='home__social-link' target="_blank">
+            <FontAwesomeIcon icon={faYoutube} />
         </a>
 
         <a href='https://www.twitter.com' className='home__social-link' target="_blank">
             <FontAwesomeIcon icon={faTwitter} />
         </a>
 
-        <a href='https://www.behance.net' className='home__social-link' target="_blank">
-            <FontAwesomeIcon icon={faBehance} />
+        <a href='https://www.linkedin.com/in/amit-goswami1/' className='home__social-link' target="_blank">
+            <FontAwesomeIcon icon={faLinkedin} />
         </a>
 
         <a href='https://dribbble.com' className='home__social-link' target="_blank">
