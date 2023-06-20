f857e9cbf0dbac9519b86c2cacd3eb2102ee8cb2
Amit Kumar Goswami <cseiubatgoswami@gmail.com>
2023-06-20 12:51:53 +0600

    add multivendor

--- src/Components/Portfolio/Menu.js
+++ src/Components/Portfolio/Menu.js
@@ -4,44 +4,52 @@ import work3 from '../../assets/3-project.JPG';
 import work4 from '../../assets/4-project.JPG';
 import work5 from '../../assets/5-project.JPG';
 import work6 from '../../assets/6-project.JPG';
+import work7 from '../../assets/7-project.PNG';
 
 const Menu = [
     {
         id: 1,
+        image: work7,
+        name: "Multivendor Ecommerce Website",
+        language: "Python(Django), Tailwind CSS, Javascript, PostgreSQL",
+        project: "https://github.com/1234amit/Full-Ecommerce-frontend"
+    },
+    {
+        id: 2,
         image: work1,
         name: "Full Ecommerce Project",
         language: "Python(Django), React js, Redux, Bootstrap",
         project: "https://github.com/1234amit/Full-Ecommerce-frontend"
     },
     {
-        id: 2,
+        id: 3,
         image: work2,
         name: "Online Marketplace",
         language: "Tailwind CSS, Django, sqlite",
         project: "https://github.com/1234amit/OnlineMarketPlace"
     },
     {
-        id: 3,
+        id: 4,
         image: work3,
         name: "Appointment Management System",
         language: "React js, Firebase, Express js, Mongo DB",
     },
     {
-        id: 4,
+        id: 5,
         image: work4,
         name: "BDMUAYTHAI Project Design",
         language: "React js, Bootstrap",
         project: "https://bangladeshmuaythai.netlify.app/"
     },
     {
-        id: 5,
+        id: 6,
         image: work5,
         name: "Sass Project Design",
         language: "SASS, HTML, CSS, Jquery",
         project: "https://sass-full-project-design.netlify.app/"
     },
     {
-        id: 6,
+        id: 7,
         image: work6,
         name: "Coffee Shop Project Design",
         language: "HTML, CSS, Javascript Jquery",
